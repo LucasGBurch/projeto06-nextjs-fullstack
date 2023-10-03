7fad08a62f183a4f64f01dad3230c861af9097ed
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-10-03 10:55:15 -0300

    Permissão de acesso ao calendário
    Aula que necessita pesquisa de Escopos do OAuth para APIs do Google. Para achar o link que vai em scope, procurando a palavra-chave calendar e userinfo. Depois, fazemos um tratamento para caso a gente não autorize no login o uso de calendário através dos callbacks. Lembrando que, para fazer um login novo precisamos ir para nossa google account e excluir o acesso do ignite call.

--- ignite-call/src/pages/api/auth/[...nextauth].api.ts
+++ ignite-call/src/pages/api/auth/[...nextauth].api.ts
@@ -6,8 +6,26 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+      authorization: {
+        params: {
+          scope:
+            'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar',
+        },
+      },
     }),
   ],
+
+  callbacks: {
+    async signIn({ account }) {
+      if (
+        !account?.scope?.includes('https://www.googleapis.com/auth/calendar')
+      ) {
+        return '/register/connect-calendar/?error=permissions' // Aqui já é false
+      } // Redireciona quando não autorizamos o calendar durante a autenticação
+
+      return true
+    },
+  },
 }
 
 export default NextAuth(authOptions)
