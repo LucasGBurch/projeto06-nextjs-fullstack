38e52b71d136187bba4e273f1ab8efa23c576606
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-10-06 14:02:37 -0300

    Criando o usuário no adapter
    Usando conceitos relacionados ao advanced initialization da documentação: https://next-auth.js.org/configuration/initialization
    Eles foram necessários para que pudéssemos acessar o ID do usuário salvo nos cookies. Só segue tendo problema com avatar_url ficando null.

--- ignite-call/src/pages/api/auth/[...nextauth].api.ts
+++ ignite-call/src/pages/api/auth/[...nextauth].api.ts
@@ -1,34 +1,52 @@
 import { PrismaAdapter } from '@/src/lib/auth/prisma-adapter'
+import { NextApiRequest, NextApiResponse } from 'next'
 import NextAuth, { NextAuthOptions } from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
+import GoogleProvider, { GoogleProfile } from 'next-auth/providers/google'
 
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(),
+export function buildNextAuthOptions(
+  req: NextApiRequest,
+  res: NextApiResponse,
+): NextAuthOptions {
+  return {
+    adapter: PrismaAdapter(req, res),
 
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
-      authorization: {
-        params: {
-          scope:
-            'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar',
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+        authorization: {
+          params: {
+            scope:
+              'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar',
+          },
         },
+
+        profile: (profile: GoogleProfile) => {
+          return {
+            id: profile.sub,
+            name: profile.name,
+            username: '', // Vem da nossa app
+            email: profile.email,
+            avatar_url: profile.picture,
+          }
+        },
+      }),
+    ],
+
+    callbacks: {
+      async signIn({ account }) {
+        if (
+          !account?.scope?.includes('https://www.googleapis.com/auth/calendar')
+        ) {
+          return '/register/connect-calendar/?error=permissions' // Aqui já é false
+        } // Redireciona quando não autorizamos o calendar durante a autenticação
+
+        return true
       },
-    }),
-  ],
-
-  callbacks: {
-    async signIn({ account }) {
-      if (
-        !account?.scope?.includes('https://www.googleapis.com/auth/calendar')
-      ) {
-        return '/register/connect-calendar/?error=permissions' // Aqui já é false
-      } // Redireciona quando não autorizamos o calendar durante a autenticação
-
-      return true
     },
-  },
+  }
 }
 
-export default NextAuth(authOptions)
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  return await NextAuth(req, res, buildNextAuthOptions(req, res))
+}
