932ca31a30055ae03c157b8b8bde1212e6c78b94
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-10-21 16:05:00 -0300

    Estrutura visual do calendário

--- ignite-call/src/utils/get-week-days.ts
+++ ignite-call/src/utils/get-week-days.ts
@@ -1,10 +1,18 @@
-export function getWeekDays() {
+interface GetWeekDaysParams {
+  short?: boolean
+}
+
+export function getWeekDays({ short = false }: GetWeekDaysParams) {
   const formatter = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' }) // long == escrito por extenso
 
   // keys() retorna os índices de cada undefined desse array. Essa função vai funcionar através de índice [] em vez de parâmetros na chamada:
   return Array.from(Array(7).keys())
     .map((day) => formatter.format(new Date(Date.UTC(2021, 5, day))))
     .map((weekDay) => {
+      if (short) {
+        return weekDay.substring(0, 3).toUpperCase()
+      }
+
       return weekDay.substring(0, 1).toUpperCase().concat(weekDay.substring(1))
     }) // Último map para os dias da semana retornados começarem com letra maiúscula.
 }
