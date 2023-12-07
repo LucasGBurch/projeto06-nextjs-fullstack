fd6ced81fb5ee56445b704448c35f3bf07807cdf
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-12-07 07:34:13 -0300

    Mais ajustes da revisão de módulo

--- ignite-call/src/pages/api/auth/[...nextauth].api.ts
+++ ignite-call/src/pages/api/auth/[...nextauth].api.ts
@@ -17,7 +17,7 @@ export function buildNextAuthOptions(
         authorization: {
           params: {
             scope:
-              'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar',
+              'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
           },
         },
 
