1bd1fc421ebe5b3329df8ddb0f32d1c4967f7c4b
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-12-21 18:29:27 -0300

    Finalizando cálculo de dias e exibindo em tela
    Essa aula inclui colocar os dias do mês seguinte, de forma similar a realizada para os dias do mês anterior. Tudo para preencher completamente o calendário e deixando desabilitados os dias dos meses anteriores e seguintes. Por fim, a lógica é completada com uma interface e variável para dividir esses arrays de dias em semanas de 7 dias, para facilitar o Map dos componentes CalendarDay, que possuem a propriedade disabled usada para desabilitar os dias de meses anteriores/seguintes.

--- ignite-call/src/components/Calendar/index.tsx
+++ ignite-call/src/components/Calendar/index.tsx
@@ -11,6 +11,16 @@ import { getWeekDays } from '@/src/utils/get-week-days'
 import { useMemo, useState } from 'react'
 import dayjs from 'dayjs'
 
+interface CalendarWeek {
+  week: number
+  days: Array<{
+    date: dayjs.Dayjs
+    disabled: boolean
+  }>
+}
+
+type CalendarWeeks = CalendarWeek[]
+
 export function Calendar() {
   const [currentDate, setCurrentDate] = useState(() => {
     return dayjs().set('date', 1) // , 1 para mês e ano, sem o dia
@@ -55,7 +65,50 @@ export function Calendar() {
       })
       .reverse()
 
-    return [...previousMonthFillArray, ...daysInMonthArray]
+    // Settando dia da data no total de dias do mês
+    const lastDayInCurrentMonth = currentDate.set(
+      'date',
+      currentDate.daysInMonth(),
+    )
+    const lastWeekDay = lastDayInCurrentMonth.get('day')
+
+    const nextMonthFillArray = Array.from({
+      length: 7 - (lastWeekDay + 1), // Começa do 0, o +1 deixa mais claro
+    }).map((_, i) => {
+      return lastDayInCurrentMonth.add(i + 1, 'day')
+    })
+
+    // Array completo desabilitando os outros meses com map:
+    const calendarDays = [
+      ...previousMonthFillArray.map((date) => {
+        return { date, disabled: true }
+      }),
+      ...daysInMonthArray.map((date) => {
+        return { date, disabled: false }
+      }),
+      ...nextMonthFillArray.map((date) => {
+        return { date, disabled: true }
+      }),
+    ]
+
+    // Agora divisão do array em semanas (_ é cada date, não usaremos, weeks é que iremos manipular aqui para criar as linhas de semanas; original é o calendarDays, mas que podemos modificar sem alterar a variável original)
+    const calendarWeeks = calendarDays.reduce<CalendarWeeks>(
+      (weeks, _, i, original) => {
+        const isNewWeek = i % 7 === 0 // Até fechar 7 dias não acaba a semana
+
+        if (isNewWeek) {
+          weeks.push({
+            week: i / 7 + 1, // Semana começa em 1 em vez de 0
+            days: original.slice(i, i + 7),
+          })
+        }
+
+        return weeks
+      },
+      [],
+    )
+
+    return calendarWeeks
   }, [currentDate])
 
   console.log(calendarWeeks)
@@ -86,44 +139,21 @@ export function Calendar() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td>
-              <CalendarDay>1</CalendarDay>
-            </td>
-            <td>
-              <CalendarDay disabled>2</CalendarDay>
-            </td>
-            <td>
-              <CalendarDay>3</CalendarDay>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <CalendarDay>1</CalendarDay>
-            </td>
-            <td>
-              <CalendarDay>1</CalendarDay>
-            </td>
-            <td>
-              <CalendarDay>1</CalendarDay>
-            </td>
-            <td>
-              <CalendarDay>1</CalendarDay>
-            </td>
-            <td>
-              <CalendarDay>1</CalendarDay>
-            </td>
-            <td>
-              <CalendarDay disabled>2</CalendarDay>
-            </td>
-            <td>
-              <CalendarDay>3</CalendarDay>
-            </td>
-          </tr>
+          {calendarWeeks.map(({ week, days }) => {
+            return (
+              <tr key={week}>
+                {days.map(({ date, disabled }) => {
+                  return (
+                    <td key={date.toString()}>
+                      <CalendarDay disabled={disabled}>
+                        {date.get('date')}
+                      </CalendarDay>
+                    </td>
+                  )
+                })}
+              </tr>
+            )
+          })}
         </tbody>
       </CalendarBody>
     </CalendarContainer>
