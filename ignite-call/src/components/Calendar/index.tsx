94544fe2c41c428d0b276a6f667f52b355684f98
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-12-22 15:12:35 -0300

    Preparando o calendário
    Aula para ajustar a desabilitação dos dias. Os dias que já passaram no mês atual devem ficar desabilitados também, já que não faz sentido marcar uma data que já passou. Além disso, Adicionamos nos dias renderizados o método onDateSelected para abrir os horários ao clicar num dia (o time picker)

--- ignite-call/src/components/Calendar/index.tsx
+++ ignite-call/src/components/Calendar/index.tsx
@@ -21,7 +21,12 @@ interface CalendarWeek {
 
 type CalendarWeeks = CalendarWeek[]
 
-export function Calendar() {
+interface CalendarProps {
+  selectedDate?: Date | null
+  onDateSelected: (date: Date) => void
+}
+
+export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
   const [currentDate, setCurrentDate] = useState(() => {
     return dayjs().set('date', 1) // , 1 para mês e ano, sem o dia
   })
@@ -84,7 +89,11 @@ export function Calendar() {
         return { date, disabled: true }
       }),
       ...daysInMonthArray.map((date) => {
-        return { date, disabled: false }
+        return {
+          date,
+          // Se a data do fim do dia for anterior a uma new Date() atual, ou seja, já passou, então a data é desabilitada:
+          disabled: date.endOf('day').isBefore(new Date()),
+        }
       }),
       ...nextMonthFillArray.map((date) => {
         return { date, disabled: true }
@@ -145,7 +154,10 @@ export function Calendar() {
                 {days.map(({ date, disabled }) => {
                   return (
                     <td key={date.toString()}>
-                      <CalendarDay disabled={disabled}>
+                      <CalendarDay
+                        onClick={() => onDateSelected(date.toDate())}
+                        disabled={disabled}
+                      >
                         {date.get('date')}
                       </CalendarDay>
                     </td>
