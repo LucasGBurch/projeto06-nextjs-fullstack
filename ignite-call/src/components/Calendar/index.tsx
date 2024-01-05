48c438c6bd901177a01b3334c79a21db568e5c9b
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2024-01-05 16:52:36 -0300

    Finalizando Query de dias lotados

--- ignite-call/src/components/Calendar/index.tsx
+++ ignite-call/src/components/Calendar/index.tsx
@@ -70,7 +70,7 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
       const response = await api.get(`/users/${username}/blocked-dates`, {
         params: {
           year: currentDate.get('year'),
-          month: currentDate.get('month'),
+          month: String(currentDate.get('month') + 1).padStart(2, '0'),
         },
       })
 
@@ -127,7 +127,8 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
           // Se a data do fim do dia for anterior a uma new Date() atual, ou seja, já passou, então a data é desabilitada:
           disabled:
             date.endOf('day').isBefore(new Date()) ||
-            blockedDates.blockedWeekDays.includes(date.get('day')),
+            blockedDates.blockedWeekDays.includes(date.get('day')) ||
+            blockedDates.blockedDates.includes(date.get('date')),
         }
       }),
       ...nextMonthFillArray.map((date) => {
--- ignite-call/src/pages/api/users/[username]/blocked-dates.api.ts
+++ ignite-call/src/pages/api/users/[username]/blocked-dates.api.ts
@@ -45,13 +45,27 @@ export default async function handle(
     )
   })
 
-  const blockedDatesRaw = await prisma.$queryRaw`
-    SELECT *
+  const blockedDatesRaw: Array<{ date: number }> = await prisma.$queryRaw`
+    SELECT
+      EXTRACT(DAY FROM S.DATE) AS date,
+      COUNT(S.date) AS amount,
+      ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60) AS size 
+
     FROM schedulings S
 
-    WHERE S.user_id = ${user.id}
-      AND DATE_FORMAT(S.date, "%Y-%m") = ${`${year}-${month}`} 
-  ` // Buscar os schedulings, mas somente os do usuário (com id correspondente)
+    LEFT JOIN user_time_intervals UTI
+      ON UTI.week_day = WEEKDAY(DATE_ADD(S.date, INTERVAL 1 DAY))
 
-  return res.json({ blockedWeekDays, blockedDatesRaw })
+    WHERE S.user_id = ${user.id}
+      AND DATE_FORMAT(S.date, "%Y-%m") = ${`${year}-${month}`}
+
+    GROUP BY EXTRACT(DAY FROM S.DATE),
+      ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60)
+
+    HAVING amount >= size
+  `
+  // Buscar os schedulings, mas somente os do usuário (com id correspondente)
+  const blockedDates = blockedDatesRaw.map((item) => item.date)
+
+  return res.json({ blockedWeekDays, blockedDates })
 }
